4979b6bda6a20d10d9fdae150e69794f4e6f9060
maram-ra <maramraddad33@gmail.com>
2025-04-08 08:21:07 +0000

    Add

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -37,3 +37,44 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 			if (i != j)
 				print_array(array, size);
 			i++;
+		}
+	}
+	if (i != high)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * quicksort_recursive - Recursive helper for quick_sort
+ * @array: Array of ints
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ */
+void quicksort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
+
+		quicksort_recursive(array, low, pivot - 1, size);
+		quicksort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Main quick sort function
+ * @array: Array to sort
+ * @size: Number of elements in array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quicksort_recursive(array, 0, size - 1, size);
+}
+
